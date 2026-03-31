421f332b0234c3d0ae812352dd308f0b3cf7fe86
Jiwon Oh <ohjiwon3040@gmail.com>
2026-03-31 15:14:43 +0900

    Upload Js exercises

---
+++ section01/chapter05.js
@@ -0,0 +1,17 @@
+//1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+//2. String Type
+let myName = "이정환";
+let myLocation = "목동";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+console.log(introduceText);
