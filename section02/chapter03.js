1007123ac720b43e10deaf94a8b15b8907029279
Jiwon Oh <ohjiwon3040@gmail.com>
2026-08-05 01:06:03 +0900

    Upload Js exercises

---
+++ section02/chapter03.js
@@ -0,0 +1,26 @@
+// 1. 배열의 구조 분해 할당
+let arr = [1, 2, 3];
+
+let [one, two, three, four = 4] = arr;
+
+// 2. 객체의 구조 분해 할당
+let person = {
+    name: "오지원",
+    age: 25,
+    hobby: "수영",
+};
+
+let { 
+    name, 
+    age : myAge, 
+    hobby, 
+    extra = "hello" 
+} = person;
+console.log(name, myAge, hobby, extra);
+
+// 3. 객체 구조 분해 할당을 이용해서 함수의 매개변수를 받는 방법
+const func = ({ name, age, hobby, extra }) => {
+    console.log(name, age, hobby, extra);
+};
+
+func(person);
